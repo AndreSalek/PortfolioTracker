d4b87379430909f0e013402c2c2c21e0fdeb52b4
AndreSalek <andre.salek00@gmail.com>
2024-07-01 18:52:30 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace PortfolioTracker.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+    }
+}
