36cc9ce0d63af4cad6dd567a95c2e32ce2f88533
AndreSalek <andre.salek00@gmail.com>
2024-08-05 14:33:51 +0200

    Add yaml modification, install dependency

--- PortfolioTracker/Program.cs
+++ PortfolioTracker/Program.cs
@@ -25,7 +25,7 @@ namespace PortfolioTracker
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                .AddEntityFrameworkStores<ApplicationDbContext>();
             // Add services to the container.
-            builder.Services.AddControllersWithViews();
+            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
             builder.Services.AddSingleton<Dictionary<Enum, string[]>>(platformKeyMap);
             builder.Services.AddHttpClient("Kraken", httpClient =>
             {
@@ -53,7 +53,7 @@ namespace PortfolioTracker
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseDeveloperExceptionPage();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
---
+++ PortfolioTrackerTests/KrakenTests.cs
@@ -0,0 +1,29 @@
+using PortfolioTracker.Models;
+
+namespace PortfolioTrackerTests
+{
+    public class KrakenTests
+    {
+        [Fact]
+        public void PlatformRequestSigner_KrakenSignatureSuccess()
+        {
+            string privateKey = "kQH5HW/8p1uGOVjbgWA7FunAmGO8lsSUXNsu3eow76sz84Q18fWxnyRzBHCd3pd5nE9qa99HAZtuZuj6F1huXg==";
+            long nonce = 1616492376594;
+            string actionPath = "/0/private/AddOrder";
+            Dictionary<string, string> payload = new Dictionary<string, string>()
+            {
+                { "nonce", "1616492376594"},
+                { "ordertype", "limit"},
+                { "pair", "XBTUSD"},
+                { "type", "buy"},
+                { "volume", "1.25"},
+            };
+            string apiSignExpected = "4/dpxb3iT4tp/ZCVEwSnEsLxx0bqyhLpdfOpc6fn7OR8+UClSV5n9E6aSS8MPtnRfp32bAb0nmbRn6H8ndwLUQ==";
+
+
+            string apiSignGenerated = PlatformRequestSigner.GetKrakenSignature(privateKey, nonce, actionPath, payload);
+
+            Assert.Equal(apiSignExpected, apiSignGenerated);
+        }
+    }
+}
